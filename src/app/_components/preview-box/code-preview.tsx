aadf209da2b25f1819905239ff86bf0a48442fa1
Jeff-Maina <devyoungblud@gmail.com>
2024-10-29 20:13:51 +0300

    feat: add code preview component

--- src/app/_components/preview-box/code-preview.tsx
+++ src/app/_components/preview-box/code-preview.tsx
@@ -1,3 +1,13 @@
+import { TFormData } from "@/app/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useEffect, useState } from "react";
+import { createHighlighter } from "shiki";
+
+type TCodePreviewProps = {
+  formData: TFormData;
+};
+
+const formCode = `
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import JSONPreview from "./json-preview";
 import { TFormData } from "@/app/types";
@@ -12,7 +22,7 @@ const Previewbox = ({ formData, children }: TPreviewProps) => {
   return (
     <div className="h-full w-full">
       <Tabs defaultValue="preview" className="w-full">
-        <TabsList className=" grid grid-cols-3">
+        <TabsList className="w- grid grid-cols-3">
           <TabsTrigger
             className="data-[state=active]:bg-white hover:bg-neutral-100 transition-all !text-neutral-800"
             value="preview"
@@ -45,3 +55,38 @@ const Previewbox = ({ formData, children }: TPreviewProps) => {
 };
 
 export default Previewbox;
+
+`;
+
+const CodePreview = ({ formData }: TCodePreviewProps) => {
+  const [highlightedCode, setHighlightedCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadHighlighter = async () => {
+      const highlighter = await createHighlighter({
+        themes: ["vitesse-dark"],
+        langs: ["typescript"],
+      });
+
+      const code = highlighter.codeToHtml(formCode, {
+        theme: "vitesse-dark",
+        lang: "typescript",
+      });
+
+      setHighlightedCode(code);
+    };
+
+    loadHighlighter();
+  }, []);
+
+  if (!highlightedCode) return <p>Loading...</p>; // Optional: Add a loading state
+
+  return (
+    <div
+      className="w-full h-full overflow-auto p-4 border rounded-md"
+      dangerouslySetInnerHTML={{ __html: highlightedCode }}
+    />
+  );
+};
+
+export default CodePreview;
--- src/app/_components/preview-box/json-preview.tsx
+++ src/app/_components/preview-box/json-preview.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { TFormData } from "@/app/types";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
@@ -5,10 +7,10 @@ type TJsonPreview = {
   formData: TFormData;
 };
 
-const JSONPreview = ({ formData }: { formData: TFormData }) => {
+const JSONPreview = ({ formData }: TJsonPreview) => {
   return (
-    <ScrollArea className="max-h-[90vh] flex flex-col p-4 overflow-x-auto">
-      <pre className="whitespace-pre-wrap font-mono ">
+    <ScrollArea className="max-h-[90vh] flex flex-col p-4  border bg-white rounded-md overflow-x-auto">
+      <pre className="whitespace-pre-wrap font-mono text-sm ">
         {JSON.stringify(formData, null, 2)}
       </pre>
     </ScrollArea>
