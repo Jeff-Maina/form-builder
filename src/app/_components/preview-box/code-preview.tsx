75cb6994d0b79b61bbba6c202daf66352682a85e
Jeff-Maina <devyoungblud@gmail.com>
2024-10-29 22:25:23 +0300

    feat: add code preview ui

--- src/app/_components/preview-box/code-preview.tsx
+++ src/app/_components/preview-box/code-preview.tsx
@@ -58,8 +58,78 @@ export default Previewbox;
 
 `;
 
+const removeDuplicates = (data: any) => [...new Set(data)];
+
+const getSourceCode = (formData: TFormData) => {
+  const FormProperties = formData.properties;
+
+  console.log(FormProperties);
+
+  const formImports: Record<
+    string,
+    {
+      component: string;
+      primitives: string[];
+    }
+  > = {
+    input: {
+      component: "input",
+      primitives: ["Input"],
+    },
+    range: {
+      component: "range",
+      primitives: ["Range"],
+    },
+    text_box: {
+      component: "text-area",
+      primitives: ["Textarea"],
+    },
+    date_picker: {
+      component: "calendar",
+      primitives: ["Calendar"],
+    },
+    dropdown: {
+      component: "dropdown",
+      primitives: ["DropdownMenu"],
+    },
+    multiple_choice: {
+      component: "radio-group",
+      primitives: ["RadioGroup"],
+    },
+    checkbox: {
+      component: "checkbox",
+      primitives: ["Checkbox"],
+    },
+    switch: {
+      component: "switch",
+      primitives: ["Switch"],
+    },
+  };
+
+  // imports
+
+  const elementImports = removeDuplicates(
+    FormProperties.map((prop) => {
+      const element = formImports[prop.type];
+      if (prop.type.includes("input")) {
+        return `import { Input } from "@/components/ui/input"; \n`;
+      } else {
+        return `import { ${element.primitives
+          .map((primitive) => `${primitive} `)
+          .join(", ")} } from "@/components/ui/${element.component}"; \n`;
+      }
+    })
+  ).join("");
+
+  // validations
+  const validations = ``;
+
+  return `${elementImports}`;
+};
+
 const CodePreview = ({ formData }: TCodePreviewProps) => {
-  return <Codeblock lang="typescript" formCode={formCode} />;
+  const sourceCode = getSourceCode(formData);
+  return <Codeblock lang="typescript" formCode={sourceCode} />;
 };
 
 export default CodePreview;
